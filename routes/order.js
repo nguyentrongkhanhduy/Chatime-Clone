fb2d3a1dc9453aeddd889d988f90818abedf6054
duyntk2 <duyntk2@vng.com.vn>
2024-10-07 16:01:33 -0400

    add some more code to order page

--- routes/order.js
+++ routes/order.js
@@ -4,16 +4,31 @@ const router = express.Router();
 const Item = require("../models/MenuItem");
 const Order = require("../models/Orders");
 
-router.get("/:id", async (req, res) => {
+router.get("/:id/:ordered?", async (req, res) => {
   try {
     const thisItem = await Item.find({ _id: req.params.id });
     console.log(thisItem[0]);
     if (thisItem.length !== 0) {
-      return res.render("order.ejs", {
-        orderingItem: thisItem[0],
-      });
+      if (req.params.ordered) {
+      } else {
+        return res.render("order.ejs", {
+          orderingItem: thisItem[0],
+          ordered: false,
+        });
+      }
     } else {
       return;
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.post("/:id/:ordered?", async (req, res) => {
+  try {
+    if (req.params.ordered) {
+      
+    } else {
     }
   } catch (error) {
     return res.status(500).send(error.message);
